93569624c735abe194fe59430e6ddef6dab13765
Andrey0189 <varnavsky06@gmail.com>
2025-12-21 13:37:21 +0000

    fix

--- hosts/nixos/local-packages.nix
+++ hosts/nixos/local-packages.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   nixpkgs.config.allwUnfree = true;
-  environments.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
       gnumake
       busybox
       gnumake
